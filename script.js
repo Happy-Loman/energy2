778f2a6f775a52955aec1da9e9cd36bec4f0b0b8
Happy-Loman <mrhappyloman@gmail.com>
2015-12-29 04:45:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-//Get the angle of object1 against object2
-function return_angle(object1_x, object1_y, object2_x, object2_y){
-    var dx = object1_x - object2_x;
-    var dy = object1_y - object2_y;
+//Get the angle of object against object2
+function return_angle(object_x, object_y, object2_x, object2_y){
+    var dx = object_x - object2_x;
+    var dy = object_y - object2_y;
     
     var angle = Math.atan2(dy, dx);
     return angle;
 }
 
-//Get the distance between object1 and object2
-function return_distance(object1_x, object1_y, object2_x, object2_y){
-    var dx = object1_x - object2_x;
-    var dy = object1_y - object2_y;
+//Get the distance between object and object2
+function return_distance(object_x, object_y, object2_x, object2_y){
+    var dx = object_x - object2_x;
+    var dy = object_y - object2_y;
     var d = Math.sqrt(dx * dx + dy * dy);
 
     return d;
@@ -48,53 +48,105 @@ var parts = {
   amount: 0,
 }
 
+var click_delay = 0;
 var autoClick = false;
+var convertAll = false;
+
 var resources = {
   batteries: {
     resource: "Battery",
+    types:{
+      first: "resources",
+      second: "energy",
+    },
     amount:0,
-    rarity:30,
+    rarity:100,
     produce_rate:0.23,
     progress : {
       clicked: false,
       converting : false,
     },
-    secs: 3,
     id:"batteries",
   },
 
   wires: {
     resource: "Wires",
+    types:{
+      first: "resources",
+      second: "energy",
+    },
     amount:0,
-    rarity:20,
+    rarity:80,
     produce_rate:0.554,
     progress : {
       clicked: false,
       converting : false,
     },
-    secs: 3,
     id:"wires",
   },
 
   lightbulbs: {
     resource: "Lightbulbs",
+    types:{
+      first: "resources",
+      second: "energy",
+    },
     amount:0,
-    rarity:10,
+    rarity:75,
     produce_rate:1,
     progress : {
       clicked: false,
       converting : false,
     },
-    secs: 3,
     id:"lightbulb",
   },
+
+  water: {
+    resource: "Water",
+    types:{
+      first: "resources",
+      second: "life",
+    },
+    amount:10,
+    rarity:16,
+    produce_rate:10,
+    progress : {
+      clicked: false,
+      converting : false,
+    },
+    id:"water",
+  },
+
+  animals: {
+    resource: "Animals",
+    //Add more type later//
+    types:{
+      first: "resources",
+      second: "life",
+    },
+    amount:0,//pounds//
+    rarity:10,
+    produce_rate:0.04,
+    progress : {
+      clicked: false,
+      converting : false,
+    },
+    id:"animals",
+  },
 }
-var resources_ = [resources.batteries, resources.wires, resources.lightbulbs];
+var resources_ = [resources.batteries, resources.wires, resources.lightbulbs, resources.water, resources.animals];
 function convert(object){
-  object.amount -= 1;
-  parts.amount -= 1;
-  cell.energy += object.produce_rate;
+  if(object.types.second == "energy" && !convertAll){
+    object.amount -= 1;
+    parts.amount -= 0.5;
+    cell.energy += object.produce_rate;
+  } else if(convertAll){
+    cell.energy += object.amount * object.produce_rate;
+    parts.amount -= object.amount * 0.2;
+    object.amount -= object.amount;
+  }
 }
+
 function update_resources(){
   for(var i = 0; i < resources_.length; i++){
     var res = resources_[i];
@@ -103,21 +155,28 @@ function update_resources(){
     }
     if(res.amount > 0 && res.progress.clicked){
       $("#" + res.id + "_bar").css("background-color", "rgb(100, 205, 100)");
+      $("#" + res.id + "_bar").css("color", "rgb(246, 246, 246)");
       convert(res);
       res.progress.converting = true;
     }
     if(res.progress.clicked == false) {
       $("#" + res.id + "_bar").css("background-color", "rgb(205, 60, 60)");
+      $("#" + res.id + "_bar").css("color", "rgb(246, 246, 246)");
     };
 
     if(res.amount <= 0) {
       res.progress.clicked = false;
       $("#" + res.id + "_bar").css("background-color", "transparent");
+      $("#" + res.id + "_bar").css("color", "#222222");
     } else if(autoClick == true){
       res.progress.clicked = true;
     }
 
-    GID(res.id + "_bar").innerHTML = res.resource + ":"+ res.amount.toFixed(2);
+    if(res.type == "energy"){
+      GID(res.id + "_bar").innerHTML = res.resource + ":"+ res.amount.toFixed(2);
+    } else {
+      GID(res.id + "_bar").innerHTML = res.resource + ":"+ res.amount.toFixed(2);
+    }
   }
 }
 
@@ -125,56 +184,58 @@ var engines = {
   generators: {
     resource: "Generator",
     amount:0,
-    rarity:5,
-    life_time:3,
-    produce_rate:0.2,
+    rarity:40,
+    life_time:30,
+    produce_rate:0.8,
     progress : {
       clicked: false,
       converting : false,
     },
-    secs: 3,
     id:"generators",
   },
 
   chargers: {
     resource: "Charger",
     amount:0,
-    rarity:4,
-    life_time:4,
-    produce_rate:0.4,
+    rarity:35,
+    life_time:20,
+    produce_rate:1,
     progress : {
       clicked: false,
       converting : false,
     },
-    secs: 3,
     id:"chargers",
   },
 
   heaters: {
     resource: "Heat",
     amount:0,
-    rarity:2,
-    life_time:5,
-    produce_rate:0.5,
+    rarity:20,
+    life_time:10,
+    produce_rate:1.5,
     progress : {
       clicked : false, 
       converting : false,
     },
-    secs: 3,
     id:"heaters",
   },
 }
 
-//These generate energy//
+//These generate energy and dont die instantly//
 var engines_ = [engines.generators, engines.chargers, engines.heaters];
 function convert_engine(object){
-  if(object.life_time <= 0){
+  if(convertAll){
+    cell.energy += (object.amount * object.life_time) * object.produce_rate;
+    parts.amount -= object.amount * 0.2;
+    object.amount -= object.amount;
+  } else if(object.life_time <= 0){
     object.amount -= 1;
-    parts.amount -= 1;
+    parts.amount -= 0.5;
     object.life_time = 10;
+  } else {
+    cell.energy += object.produce_rate;
+    object.life_time -= 0.2;
   }
-  cell.energy += object.produce_rate;
-  object.life_time -= 0.05;
 }
 function update_engines(){
   for(var i = 0; i < engines_.length; i++){
@@ -184,16 +245,20 @@ function update_engines(){
     }
     if(eng.amount > 0 && eng.progress.clicked){
       $("#" + eng.id + "_bar").css("background-color", "rgb(100, 205, 100)");
+      $("#" + eng.id + "_bar").css("color", "rgb(240, 240, 240)");
       convert_engine(eng);
       eng.progress.converting = true;
       //console.log(eng.amount);
     }
     if(eng.progress.clicked == false) {
       $("#" + eng.id + "_bar").css("background-color", "rgb(205, 60, 60)");
+      $("#" + eng.id + "_bar").css("color", "rgb(240, 240, 240)");
     }
 
     if(eng.amount <= 0) {
+      eng.progress.clicked = false;
       $("#" + eng.id + "_bar").css("background-color", "transparent");
+      $("#" + eng.id + "_bar").css("color", "#222222");
     } else if(autoClick == true){
       eng.progress.clicked = true;
     }
@@ -206,39 +271,36 @@ var forms = {
   solars: {
     resource: "Solar Energy",
     amount:0,
-    rarity:2.5,
+    rarity:6,
     produce_rate:0.1,
     progress : {
       clicked: false,
       converting : false,
     },
-    secs: 3,
     id:"solars",
   },
 
   thermal: {
     resource: "Thermal Energy",
     amount:0,
-    rarity:2,
+    rarity:4,
     produce_rate:0.22,
     progress : {
       clicked: false,
       converting : false,
     },
-    secs: 3,
     id:"thermal",
   },
 
   chemical: {
     resource: "Chemical Energy",
     amount:0,
-    rarity:1.5,
+    rarity:2,
     produce_rate:0.31,
     progress : {
       clicked: false,
       converting : false,
     },
-    secs: 3,
     id:"chemical",
   },
 
@@ -251,10 +313,10 @@ var forms = {
       clicked: false,
       converting : false,
     },
-    secs: 3,
     id:"electrical",
   },
 }
+
 //Forms of energy//
 var forms_ = [forms.solars, forms.thermal, forms.chemical, forms.electrical];
 function convert_form(object){
@@ -268,14 +330,17 @@ function update_forms(){
     }
     if(form.amount > 0 && form.progress.clicked){
       $("#" + form.id + "_bar").css("background-color", "rgb(100, 205, 100)");
+      $("#" + form.id + "_bar").css("color", "rgb(240, 240, 240)");
       convert_form(form);
       form.progress.converting = true;
     } 
     if(form.progress.clicked == false) {
       $("#" + form.id + "_bar").css("background-color", "rgb(205, 60, 60)");
+      $("#" + form.id + "_bar").css("color", "rgb(240, 240, 240)");
     };
     if(form.amount <= 0) {
       $("#" + form.id + "_bar").css("background-color", "transparent");
+      $("#" + form.id + "_bar").css("color", "#222222");
     }
     GID(form.id + "_bar").innerHTML = form.resource + ":"+ form.amount.toFixed(2);
   }
@@ -283,7 +348,7 @@ function update_forms(){
 
 var true_ = false;
 var mouse = {
-  display:"Click Here",
+  display:"",
   toolTip:false,
   disableTooltip:false,
 }
